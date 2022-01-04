33ea7dfc01d03ba37913f5f06aebab1c989a49ff
Mike Doud <mbdoud@uw.edu>
2022-01-04 12:15:18 -0800

    Add files via upload

---
+++ app.R
@@ -0,0 +1,42 @@
+library(shiny)
+
+# read in the data
+vaccine_components <- read.csv("vaccine_components.csv", header=FALSE, row.names=1) # dataframe with each row named by vaccine name
+
+# define functions to process ingredient lists from the data frame
+get_ingredients <- function(vacname) { # returns a list of ingredients for a vaccine name (in lower case, empty elements removed)
+  this_list <- tolower(as.list(vaccine_components[vacname,2:ncol(vaccine_components)]))
+  this_list <- this_list[this_list != ""] # remove empty elements from the list
+  return(trimws(this_list)) # return after trimming leading or ending white space from list elements
+} 
+get_intersect <- function(vacname1, vacname2) {intersect(get_ingredients(vacname1),get_ingredients(vacname2))} # returns the intersection of ingredients for two vaccine names
+formatted_ingredients <- function(vacname) {paste(get_ingredients(vacname), collapse=", ")}
+formatted_intersect <- function(vacname1, vacname2) {paste(get_intersect(vacname1, vacname2), collapse=", ")} # formatting the list to print separated by comma
+
+# shiny ui
+ui <- fluidPage(
+  selectInput("vaccine1", "Select first vaccine", choices = row.names(vaccine_components)),
+  selectInput("vaccine2", "Select second vaccine", choices = row.names(vaccine_components)),
+  br(),
+  textOutput(outputId="vaccine1_ingredients"),
+  br(),
+  textOutput(outputId="vaccine2_ingredients"),
+  br(),
+  textOutput(outputId="common_ingredients")
+)
+
+# shiny server
+server <- function(input,output) {
+  output$vaccine1_ingredients <- renderText({ paste("The ingredients in the first vaccine are: ", formatted_ingredients(input$vaccine1)) })
+  output$vaccine2_ingredients <- renderText({ paste("The ingredients in the second vaccine are: ", formatted_ingredients(input$vaccine2)) })
+  
+  output$common_ingredients <- renderText({ 
+    if(length(get_intersect(input$vaccine1, input$vaccine2)) > 0)
+    { paste("The ingredients in common are: ", formatted_intersect(input$vaccine1, input$vaccine2)) }
+    else
+    { paste("No ingredients in common identified.") }
+    })
+}
+
+# run the shiny app
+shinyApp(ui=ui,server=server)
